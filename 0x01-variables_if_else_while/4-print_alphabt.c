8c4cb4282338a0f20e5b1846055d4c5f91eda952
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-02-19 03:19:45 -0800

    t4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * * main - alpha print
+ * Return:0
+ */
+int main(void)
+{
+char ch = 'a';
+char end = 'z';
+for (; ch <= end; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
+
