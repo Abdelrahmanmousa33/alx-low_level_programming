b0181b001f5342418b8f113a856ca8ee33b68dc9
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-02-21 12:29:32 -0800

    tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Adds two values
+ * @n1: first nu
+ * @n2: sec num 
+ * Return: Int
+ */
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+
+}
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+int a =2;
+putchar(a+'0');
+return 0;
+}
