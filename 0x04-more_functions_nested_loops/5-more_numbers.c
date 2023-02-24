53e4874386d1df8380590e1f85153618bda43210
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-02-24 10:12:29 -0800

    t6

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * most_numbers - chexk up
+ * Return: 1(sucsses),0(otherwise)
+ */
+void most_numbers(void)
+{
+	int c;
+
+	for (
+       
+
+
+
+
+
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: input
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = n; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+
+
+
+
+
+
+}
