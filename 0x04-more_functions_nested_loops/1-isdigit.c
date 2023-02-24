aa659eeed35a6f64d47b04983f427a1aa9de48f3
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-02-24 03:36:58 -0800

    t1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,14 +4,14 @@
  * @c: the char
  * Return: 1(sucsses),0(otherwise)
  */
-int _isdigit (int c)
+int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
