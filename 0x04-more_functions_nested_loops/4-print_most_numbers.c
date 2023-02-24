ef95f43126a7c7fd071a9470dc0e656b781de514
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-02-24 09:42:58 -0800

    t4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,11 @@ void print_most_numbers(void)
 
 	for (c = 48; c <= 57; c++)
 	{
-		if (c != 50 || c != 52)
+		if (c == 50 || c == 52)
 		{
-		_putchar(c);
+		continue;
 		}
+		_putchar(c);
 	}
 	_putchar('\n');
 }
