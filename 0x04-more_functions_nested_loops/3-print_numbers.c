4dd43da5631bd1770558b52e858a466f619ef619
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-02-24 03:47:26 -0800

    t3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - chexk up
+ * Return: 1(sucsses),0(otherwise)
+ */
+int print_numbers(void)
+{
+	for (int c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+	return (0);
+}
