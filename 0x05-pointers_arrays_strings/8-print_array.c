e9e6bc57e5c200acefdffeb091660b8ff4a289e7
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-02-27 11:53:59 -0800

    t8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,14 @@ void print_array(int *a, int n)
 
 	for (; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		if (i == n - 1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
 	}
 	_putchar('\n');
 }
