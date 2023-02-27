fea801dc9bc4a47068c5c05228c62ff47737f3a1
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-02-27 08:49:20 -0800

    t4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ *@s:input
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int n = 0, b;
+	char *rev;
+
+	while (s[n] != '\0')
+	{
+		n++;
+		rev[n] = s[n];
+	}
+	for (; n >= 0; n--)
+	{
+		_putchar(rev[n]);
+	}
+	_putchar('\n');
+}
