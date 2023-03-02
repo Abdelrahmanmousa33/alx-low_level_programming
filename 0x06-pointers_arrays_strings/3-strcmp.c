7d029cbcae11ab44b048e7e1c6554db7b85729d7
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-02 00:50:34 -0800

    t3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,9 +19,9 @@ int _strcmp(char *s1, char *s2)
 
 	}
 	if (s1[c] > s2[c])
-		return (1);
+		return (s1[c] - s2[c]);
 	else if (s1[c] < s2[c])
-		return (-1);
+		return (s1[c] - s2[c]);
 	else
 		return (0);
 
