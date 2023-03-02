f51534597866971c66a0e97c15db1f133398946e
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-02 08:29:44 -0800

    t6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - check the code
+ *@ptr:pinter
+ * Return: Always 0.
+ */
+char *cap_string(char *ptr)
+{
+	int i = 0;
+
+	while (ptr[i] != '\0')
+	{
+		if (ptr[i] >= 'a' && ptr[i] <= 'z')
+		{
+			if (ptr[i - 1] == ' ' ||
+			ptr[i - 1] == '\t' ||
+			ptr[i - 1] == '\n' ||
+			ptr[i - 1] == ',' ||
+			ptr[i - 1] == ';' ||
+			ptr[i - 1] == '.' ||
+			ptr[i - 1] == '!' ||
+			ptr[i - 1] == '?' ||
+			ptr[i - 1] == '"' ||
+			ptr[i - 1] == '(' ||
+			ptr[i - 1] == ')' ||
+			ptr[i - 1] == '{' ||
+			ptr[i - 1] == '}')
+			{
+				ptr[i] -= 32;
+			}
+		}
+		i++;
+}
+
