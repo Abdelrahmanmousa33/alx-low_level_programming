f354db6eb86199d3842189c55c7d1f3d8d1c319f
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-02 07:58:41 -0800

    t5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper - check the code
+ *@ptr:input
+ * Return: Always 0.
+ */
+char *string_toupper(char *ptr)
+{
+	int i = 0;
+
+	while (ptr[i] != '\0')
+	{
+		if (ptr[i] >= 65 && ptr[i] <= 97)
+		{
+			ptr[i] = ptr[i] + 32;
+		}
+		i++;
+	}
+	return (ptr);
+}
