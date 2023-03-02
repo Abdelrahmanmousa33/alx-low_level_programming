93f1b5890524f9fdba4dbd35ec5cef99f8349adb
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-01 23:36:05 -0800

    t1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,9 +11,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int d, c;
+	int d, c, srclen;
 
 	d = strlen(dest);
+	srclen = strlen(src);
+	if (n > srclen)
+	{
+		n = strlen(src);
+	}
 	for (c = 0; c < n; c++)
 	{
 		dest[d + c] = src[c];
