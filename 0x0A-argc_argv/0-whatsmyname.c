5f69eb0a103d5cb9dd12fbb761357bdf46f0b83f
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-12 12:51:48 -0700

    t0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print its name folowed by a new line
+ *@argc:number of parameters
+ *@argv:array of strings
+ * Return: - 0
+ */
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
