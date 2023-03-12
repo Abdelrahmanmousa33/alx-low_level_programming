1fb0b0dd6d5613c12a1e7b2d1abb4eaea6a39a42
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-12 13:01:30 -0700

    t1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print its number of arguments folowed by a new line
+ *@argc:number of parameters
+ *@argv:array of strings
+ * Return: - 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
