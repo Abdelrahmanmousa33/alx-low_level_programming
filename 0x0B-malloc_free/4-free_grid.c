c90c9ba2a17af4af9df89437f59f5607e264237e
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-14 13:45:35 -0700

    t4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *free_grid - function frees a 2d array
+ *@grid:the array
+ *@height:of array
+ *Return:void
+ */
+void free_grid(int **grid, int height)
+{
+	while (height >= 0)
+	{
+		free(grid[height]);
+	}
+	free(grid);
+}
