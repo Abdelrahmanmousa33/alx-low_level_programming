beafc6a8769a7cafba50197ca4e247640058c7c8
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-15 10:22:58 -0700

    t4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print its number of arguments folowed by a new line
+ *@argc:number of parameters
+ *@argv:array of strings
+ * Return: - 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum, c;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (c = 0; c < argc; c++)
+	{
+	if (atoi(argv[c]) >= 48 || atoi(argv[c]) <= 57)
+	{
+	sum = sum + atoi(argv[c]);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
