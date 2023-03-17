9044a391e97210686edfa9e18e98702dbda8caf3
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-17 09:26:53 -0700

    t2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+/**
+ * _calloc- allocating memroy for an array
+ *@nmemb:number of elemnts
+ *@size:size of each elemnt
+ *@nmemb:number of char to be added
+ * Return: Always 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nemeb == 0 && size == 0)
+		return (NULL);
+	ptr = calloc(nmemb, size);
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
