d4dfb9979f55dfb603bf21857a0bbbe8f3dc27f7
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-21 16:46:44 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - a funcation that creat a var of type dog
+ * @name:dogs name
+ * @age:dogs age
+ * @owner: dogs owner
+ * Return:nothing
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeof(dog_t));
+	char *namecpy = malloc(sizeof(name));
+	char *ownercpy = malloc(sizeof(owner));
+
+	strcpy(namecpy, name);
+	strcpy(ownercpy, owner);
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+
+}
