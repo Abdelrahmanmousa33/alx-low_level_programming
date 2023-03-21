f488354e50b6c587b22117c9e6fd5a3a64d770f5
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-21 14:04:15 -0700

    task_0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dogs info
+ * @name:1st mem
+ * @age: 2ec mem
+ * @owner:3rd mem
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
