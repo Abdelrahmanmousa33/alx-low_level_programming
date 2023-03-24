fd4f4ee1ddeb7142bde5193cd43469b68e9d79f0
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-03-23 18:38:41 -0700

    task 2_fixedtypo

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		x = va_arg(ap, char *);
 		if (x == NULL)
-			printf("nill");
+			printf("nil");
 		else
 			printf("%s", x);
 		if (separator != NULL && i + 1 < n)
