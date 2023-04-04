652b555fb51bcc474fa821e781c4ac6ec13abe67
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-04-04 08:30:23 -0700

    task11solved

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses al list
+ * @head: pointer to apointer to alist
+ * Return: pointer to the list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *first = NULL;
+	kistint_t *secound = NULL;
+
+	while ((*head) != NULL)
+	{
+		secound = (*head)->next;
+		(*head)->next = first;
+		first = *head;
+		*head = first;
+	}
+	*head = first;
+	return (*head);
+}
