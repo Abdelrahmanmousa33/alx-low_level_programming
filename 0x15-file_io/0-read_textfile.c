a6e699eb2ef689ef439cfd38af973f9db3616b7c
Abdelrahman Mousa <abdelrahmanmousa33@gmail.com>
2023-04-18 01:16:46 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,31 +4,22 @@
 #include "main.h"
 
 /**
- * ssize_read_textfile - reads a text file and prints it to POSIX stdout
- * @filename: the file 
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: the file
  * @letters: nubmer of letters
  * Return: number of letters printed or 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp = fopen(filename,"r");
-	char ch;
+	FILE *fp = fopen(filename, "r");
+	char *buff = malloc(sizeof(char) * letters);
 	size_t lettersred = 0;
 
-	if(fp == NULL )
+	if (fp == -1)
 		return (0);
-
-	while (!feof(fp))
-	{
-		ch = getc(fp);
-		dprintf(1, "%c", ch);
-		lettersred++;
-		if (lettersred == letters)
-		{
-			fclose(fp);
-			return (lettersred);
-		}
-	}
+	lettersred += read(1, buff, letters);
+	dprintf(1, "%s", buff);
 	fclose(fp);
+	free(buff);
 	return (lettersred);
 }
