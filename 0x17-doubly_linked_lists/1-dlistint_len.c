c1903175c3add9a6604a108d679f190c2ae97e19
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-06-21 00:01:45 -0700

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - return number of elemts in a linked list
+ * @h: the list
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+int nodes_count = 0;
+
+if (h == NULL)
+	return (0);
+while (h != NULL)
+{
+	nodes_count++;
+}
+return (nodes_count);
+}
