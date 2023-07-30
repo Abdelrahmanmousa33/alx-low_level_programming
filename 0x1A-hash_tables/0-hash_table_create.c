2edc97f6bd0fe01818d0aa041d5b00e2823df1ef
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-07-30 06:16:40 -0700

    improved

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
 
-	new_hash = malloc(sizeof(hash_table_t));
+	new_hash = malloc(sizeof(hash_table_t) * size);
 	if (new_hash == NULL)
 		return (NULL);
 
