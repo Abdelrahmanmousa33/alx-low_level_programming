678295a4909d76ab3a1410623fc3f9e064ec8f44
abdelrahmanmousa <abdelrahmanmousa33@gmail.com.com>
2023-07-31 07:35:06 -0700

    improved

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,24 +10,34 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
-	unsigned long int h_code;
-	char *value_copy;
+	unsigned long int h_code, i;
+	char *value_copy, *key_copy;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-
 	value_copy = strdup(value);
-	if (value_copy == NULL)
+	key_copy = strdup(key);
+	if (value_copy == NULL || key_copy)
 		return (0);
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+	{
+		free(value_copy);
+		free(key_copy);
 		return (0);
-
+	}
 	h_code = key_index((const unsigned char *)key, ht->size);
-	new_node->key = (char *)h_code;
+	for (i = h_code; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+	new_node->key = key_copy;
 	new_node->value = value_copy;
-
 	if (ht->array[h_code] == NULL)
 	{
 		new_node->next = NULL;
